ff1e19188cd3d819adf5b0ddc783253b935f4772
Tushar Malpani <tusharmalpani20@gmail.com>
2025-12-24 23:23:53 +0530

    Update Axios baseURL for Biomax API to use HTTP endpoint and comment out cron job functions for importing and updating data from ERPNext

--- src/config/biomax.axios.ts
+++ src/config/biomax.axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const biomax_axios_instance = axios.create({
     // baseURL: 'http://localhost:82',
     //baseURL: 'http://183.82.102.253:82',
-    baseURL: 'https://smartoffice.sidsfarm.com',
+    baseURL: 'http://smartoffice.sidsfarm.com',
     params: {
         APIKey: process.env.BIOMAX_APIKEY as string
     }
