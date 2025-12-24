bee07bcfb4f599afe7ecdfeca2cdba4e23abb0a5
Tushar Malpani <tusharmalpani20@gmail.com>
2025-12-24 17:27:26 +0530

    make chunk size smaller

--- src/module/employee/service/employee.service.ts
+++ src/module/employee/service/employee.service.ts
@@ -208,7 +208,7 @@ const import_employee_to_biomax_from_erpnext_via_biomax_api = async () => {
 
 
     if (employee_with_biomax_id_list.length > 0) {
-        const chunkSize = 500;
+        const chunkSize = 50;
         for (let i = 0; i < employee_with_biomax_id_list.length; i += chunkSize) {
             const chunk = employee_with_biomax_id_list.slice(i, i + chunkSize);
             await Employee_Erp_Api.update_employee_id(chunk);
