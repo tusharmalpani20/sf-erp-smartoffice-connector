f15d16c6811ff9883df50de517aee0e0a3ee3b2a
Tushar Malpani <tusharmalpani20@gmail.com>
2025-12-24 14:55:26 +0530

    Updated baseurl

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axios_instance = axios.create({
     // baseURL: 'http://82.112.231.90:8003',
-    // baseURL: 'https://sf.hopnet.co.in',
-    baseURL: 'http://localhost:8001',
+    baseURL: 'https://erpnext.sidsfarm.com',
+    // baseURL: 'http://localhost:8001',
     headers: {
         'Authorization': `token ${process.env.ERPNEXT_APIKEY as string}`,
         'Content-Type': 'application/json'
