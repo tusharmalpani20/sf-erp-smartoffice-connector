f5ae2519c9f0f4abcc0c16be499d2870bcc0ea98
Tushar Malpani <tusharmalpani20@gmail.com>
2025-12-24 16:21:03 +0530

    Update Axios baseURL for Biomax API to use secure HTTPS endpoint

--- src/config/biomax.axios.ts
+++ src/config/biomax.axios.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const biomax_axios_instance = axios.create({
     // baseURL: 'http://localhost:82',
-    baseURL: 'http://183.82.102.253:82',
+    //baseURL: 'http://183.82.102.253:82',
+    baseURL: 'https://smartoffice.sidsfarm.com',
     params: {
         APIKey: process.env.BIOMAX_APIKEY as string
     }
